734b52213515d9c79e290552ea4d919e74080aca
whatwewant <tobewhatwewant@outlook.com>
2022-11-30 09:01:36 +0800

    chore: update string support custom letters

--- string.go
+++ string.go
@@ -6,10 +6,15 @@ import (
 
 const LETTERS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-func String(length int) string {
+func String(length int, letters ...string) string {
+	lettersX := LETTERS
+	if len(letters) > 0 && letters[0] != "" {
+		lettersX = letters[0]
+	}
+
 	b := make([]byte, length)
 	for i := range b {
-		b[i] = LETTERS[rand.Intn(len(LETTERS))]
+		b[i] = LETTERS[rand.Intn(len(lettersX))]
 	}
 	return string(b)
 }
