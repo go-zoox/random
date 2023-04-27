f00ab4bc40369a1430ea5cbf58138d0a606cc8e8
whatwewant <tobewhatwewant@outlook.com>
2023-04-27 20:57:08 +0800

    chore(client): update client id and secret generator

--- client.go
+++ client.go
@@ -1,8 +1,8 @@
 package random
 
 const DEFAULT_CLIENT_ID_LENGTH = 20
-const DEFAULT_CLIENT_SECRET_LENGTH = 32
-const DEFAULT_CLIENT_LETTERS = "abcdefghijklmnopqrstuvwxyz0123456789"
+const DEFAULT_CLIENT_SECRET_LENGTH = 40
+const DEFAULT_CLIENT_LETTERS = "abcde0123456789"
 
 func ClientID(length ...int) string {
 	lengthX := DEFAULT_CLIENT_ID_LENGTH
--- client_test.go
+++ client_test.go
@@ -6,6 +6,8 @@ func TestClient(t *testing.T) {
 	clientID := ClientID()
 	clientSecret := ClientSecret()
 
+	t.Logf("ClientID: %s, ClientSecret: %s", clientID, clientSecret)
+
 	if len(clientID) != DEFAULT_CLIENT_ID_LENGTH {
 		t.Errorf("Client ID length should be %d, got %d", DEFAULT_CLIENT_ID_LENGTH, len(clientID))
 	}
